de1a4a35bc1187237247c825d7cab34d3f335df9
GBU_Giang Nguyen <giang.nguyen@dsquare.com.vn>
2025-02-18 17:48:01 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });
